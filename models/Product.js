63b84be4511646a1607b5879dac84fd44df26097
1nkarych <1nkarychh@gmail.com>
2026-02-08 17:46:47 +0500

    Add files via upload

---
+++ models/Product.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const ProductSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    price: { type: Number, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false } // Change to false
+});
+module.exports = mongoose.model('Product', ProductSchema);
